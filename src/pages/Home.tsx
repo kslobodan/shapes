291750d348c695354a4a6ca906c68e35acc6e2f5
Slobodan Komazec <skomazec@xorantech.com>
2024-02-08 20:44:32 +0100

    small fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Footer, FooterMobile } from "../components/footer";
 import Portfolio from "../components/portfolio/Portfolio";
-import Options from "../components/Options";
+import Options from "../components/options/Options";
 import Header from "../components/header/Header";
 import Testimonials from "../components/Testimonials";
 import ThreeMinds from "../components/ThreeMinds";
