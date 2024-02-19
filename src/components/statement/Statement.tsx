79105456752bac7c7e1076ac2df50768f4467597
Slobodan Komazec <skomazec@xorantech.com>
2024-02-19 20:46:03 +0100

    small fix

--- src/components/statement/Statement.tsx
+++ src/components/statement/Statement.tsx
@@ -31,8 +31,8 @@ const Statement = () => {
       <div className={styles.content}>
         {smallScreen &&
           descriptionList.map((row, index) => (
-            <div className={styles.text__mobile}>
-              <div key={index}>{row}</div>
+            <div key={index} className={styles.text__mobile}>
+              <div>{row}</div>
             </div>
           ))}
         <div className={styles.text}>
