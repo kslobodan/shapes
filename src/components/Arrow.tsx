2fc72f55d821bf776ad2a01e5273e0ea61213f2f
Slobodan Komazec <skomazec@xorantech.com>
2024-02-27 12:13:40 +0100

    fixing fixed arrow for desktop

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -3,7 +3,7 @@ import { useAppContext } from "../customHooks/useAppContext";
 
 const Arrow = () => {
   const [isFixed, setIsFixed] = useState(false);
-  const { screenSize } = useAppContext();
+  const { screenSize, smallScreen } = useAppContext();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,17 +27,27 @@ const Arrow = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const smallScreen = [
-    "small-screen",
-    "x-small-screen",
-    "xx-small-screen",
-  ].includes(screenSize);
+  const rightValue = () => {
+    if (smallScreen) return "50%";
+    else if (screenSize === "medium-screen") return "-10px";
+    else if (screenSize === "large-screen") return "15px";
+    else if (screenSize === "x-large-screen") return "35px";
+    else return "50px";
+  };
+
+  const scaleValue = () => {
+    if (smallScreen) return "translateX(50%) scale(0.5)";
+    else if (screenSize === "medium-screen") return "scale(0.6)";
+    else if (screenSize === "large-screen") return "scale(0.73)";
+    else if (screenSize === "x-large-screen") return "scale(0.87)";
+    else return "scale(1)";
+  };
 
   const arrowStyle: CSSProperties = {
     position: "fixed",
     bottom: "0",
-    right: smallScreen ? "50%" : "50px",
-    transform: smallScreen ? " translateX(50%) scale(0.5)" : "scale(1)",
+    right: rightValue(),
+    transform: scaleValue(),
     transition: "opacity 0.5s",
     display: isFixed ? "block" : "none",
     opacity: isFixed ? 1 : 0,
